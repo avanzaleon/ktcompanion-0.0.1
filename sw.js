dfd8467b5e84fa0440ae70b2874e9bb5b8c680e0
avanzaleon <avanzaleon@gmail.com>
2026-08-15 20:22:16 +0200

    Añade Deathwatch al sistema de actualización de KT Companion

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE="kt-companion-v6";
-const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./blades.js"];
+const CACHE="kt-companion-v7-dw";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./blades.js","./deathwatch.js"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
  if(e.request.mode==="navigate"){
   e.respondWith(fetch(e.request).then(async r=>{
    const text=await r.text();
-   const injected=text.replace(/<\/body>/i,'<script src="./blades.js?v=1"></script></body>');
+   const injected=text.replace(/<\/body>/i,'<script src="./blades.js?v=1"></script><script src="./deathwatch.js?v=1"></script></body>');
    const response=new Response(injected,{status:r.status,statusText:r.statusText,headers:r.headers});
    caches.open(CACHE).then(c=>c.put("./index.html",response.clone()));
    return response;
