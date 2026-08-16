24bcbaa77b25001f8534e28cc2074387c8ea33dc
avanzaleon <avanzaleon@gmail.com>
2026-08-16 18:26:32 +0200

    Integra Wolf Scouts y actualiza cache

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE="kt-companion-v8-dw";
-const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./blades.js","./deathwatch.js"];
+const CACHE="kt-companion-v9-ws";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./blades.js","./deathwatch.js","./wolfscouts.js"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
  if(e.request.mode==="navigate"){
   e.respondWith(fetch(e.request).then(async r=>{
    const text=await r.text();
-   const injected=text.replace(/<\/body>/i,'<script src="./blades.js?v=2"></script><script src="./deathwatch.js?v=2"></script></body>');
+   const injected=text.replace(/<\/body>/i,'<script src="./blades.js?v=3"></script><script src="./deathwatch.js?v=3"></script><script src="./wolfscouts.js?v=1"></script></body>');
    const response=new Response(injected,{status:r.status,statusText:r.statusText,headers:r.headers});
    caches.open(CACHE).then(c=>c.put("./index.html",response.clone()));
    return response;
